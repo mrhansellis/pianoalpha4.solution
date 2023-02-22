9861b6cc9ed7e8e108a1d14721cb7824293434f2
Hans Ellis <bullhorn@gmail.com>
2023-02-21 20:19:35 -0800

    apply new visual design, add media player instances to play notes simultaneously, add refresh button
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

--- PianoAlpha4/MainWindow.xaml.cs
+++ PianoAlpha4/MainWindow.xaml.cs
@@ -23,7 +23,14 @@ namespace PianoAlpha4
     /// </summary>
     public partial class MainWindow : Window
     {
-        MediaPlayer playNote = new MediaPlayer();
+        MediaPlayer playC4Note = new MediaPlayer();
+        MediaPlayer playE4Note = new MediaPlayer();
+        MediaPlayer playG4Note = new MediaPlayer();
+        MediaPlayer playD4Note = new MediaPlayer();
+        MediaPlayer playF4Note = new MediaPlayer();
+        MediaPlayer playA5Note = new MediaPlayer();
+        MediaPlayer playB5Note = new MediaPlayer();
+        MediaPlayer playC5Note = new MediaPlayer();
 
         Uri C4Note;
         Uri E4Note;
@@ -38,19 +45,19 @@ namespace PianoAlpha4
         public void Animation(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            var WidthAnimation = new DoubleAnimation() { To = 150,  Duration = TimeSpan.FromSeconds(0.1), AutoReverse = true };
-            var HeightAnimation = new DoubleAnimation() { To = 150, Duration = TimeSpan.FromSeconds(0.1), AutoReverse = true };
             
+            var WidthAnimation = new DoubleAnimation() { To = 150, Duration = TimeSpan.FromSeconds(0.1), AutoReverse = true };
+            var HeightAnimation = new DoubleAnimation() { To = 150, Duration = TimeSpan.FromSeconds(0.1), AutoReverse = true };
+
             button.BeginAnimation(Button.WidthProperty, WidthAnimation);
             button.BeginAnimation(Button.HeightProperty, HeightAnimation);
-
         }
         
         public MainWindow()
         {
             InitializeComponent();
 
-            C4Note = new Uri("file:///C:/Users/bullh/source/repos/PianoAlpha4/PianoAlpha4/Resources//c4.mp3");
+            C4Note = new Uri("file:///C:/Users/bullh/source/repos/PianoAlpha4/PianoAlpha4/Resources/c4.mp3");
             E4Note = new Uri("file:///C:/Users/bullh/source/repos/PianoAlpha4/PianoAlpha4/Resources/e4.mp3");
             G4Note = new Uri("file:///C:/Users/bullh/source/repos/PianoAlpha4/PianoAlpha4/Resources/g4.mp3");
             D4Note = new Uri("file:///C:/Users/bullh/source/repos/PianoAlpha4/PianoAlpha4/Resources/d4.mp3");
@@ -63,64 +70,85 @@ namespace PianoAlpha4
         private void C_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(C4Note);
-            playNote.Play();
+            playC4Note.Open(C4Note);
+            playC4Note.Play();
             E.Visibility = Visibility.Visible;
         }
 
         private void E_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(E4Note);
-            playNote.Play();
+            playE4Note.Open(E4Note);
+            playE4Note.Play();
             G.Visibility = Visibility.Visible;
         }
 
         private void G_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(G4Note);
-            playNote.Play();
+            playG4Note.Open(G4Note);
+            playG4Note.Play();
             D.Visibility = Visibility.Visible;
         }
 
         private void D_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(D4Note);
-            playNote.Play();
+            playD4Note.Open(D4Note);
+            playD4Note.Play();
             F.Visibility = Visibility.Visible;
         }
 
         private void F_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(F4Note);
-            playNote.Play();
+            playF4Note.Open(F4Note);
+            playF4Note.Play();
             A.Visibility = Visibility.Visible;
         }
 
         private void A_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(A5Note);
-            playNote.Play();
+            playA5Note.Open(A5Note);
+            playA5Note.Play();
             B.Visibility = Visibility.Visible;
         }
 
         private void B_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(B5Note);
-            playNote.Play();
+            playB5Note.Open(B5Note);
+            playB5Note.Play();
             C5.Visibility = Visibility.Visible;
         }
 
         private void C5_Button_Click(object sender, RoutedEventArgs e)
         {
             Animation(sender, e);
-            playNote.Open(C5Note);
-            playNote.Play();
+            playC5Note.Open(C5Note);
+            playC5Note.Play();
         }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            E.Visibility = Visibility.Hidden;
+            G.Visibility = Visibility.Hidden;
+            D.Visibility = Visibility.Hidden;
+            F.Visibility = Visibility.Hidden;
+            A.Visibility = Visibility.Hidden;
+            B.Visibility = Visibility.Hidden;
+            C5.Visibility = Visibility.Hidden;
+        }
+
+        private void RefreshButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            RefreshButton.Visibility = Visibility.Visible;
+        }
+
+        //private void C_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //{
+
+        //}
     }
 }
